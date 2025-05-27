90b4ebe78ac56fea1cab38ca8f4e6688b117e7fe
esKing111 <shresthaelon5466@gmail.com>
2025-05-27 19:03:57 +1000

    opens the login screen first

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -33,8 +33,6 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
-      }}
-      initialRouteName="community">
       <Tabs.Screen
         name="community"
         options={{
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
-      <Redirect href="/(tabs)/community" />
+      <Redirect href="/(tabs)/login" />
       <StatusBar style="auto" />
     </ThemeProvider>
   );
